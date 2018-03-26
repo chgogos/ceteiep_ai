acfff46519c02d18f321bd4881b775aacd49efd3
Christos Gogos <chgogos@gmail.com>
2018-03-26 11:18:40 +0300

    hanoi

---
+++ hanoi_towers/hanoi2.py
@@ -0,0 +1,18 @@
+globvar = 0
+
+def printMove(fr,to):
+    global globvar
+    print(str(globvar) + '. move from ' + str(fr) + ' to ' + str(to))
+
+def towers(n, fr, to, spare):
+    global globvar
+    if n==1:
+        globvar += 1
+        printMove(fr, to)
+    else:
+        towers(n-1, fr, spare, to)
+        towers(1,fr,to,spare)
+        towers(n-1, spare, to, fr)
+
+# if __name__ == "__main__":
+#     towers(5, 'f', 't', 's')
