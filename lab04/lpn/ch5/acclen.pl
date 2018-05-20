05e74094253626ec2f2f1254d92a9392c2978b0c
Christos Gogos <chgogos@gmail.com>
2018-05-20 11:48:19 +0300

    lpn

--- lab04/lpn/ch5/acclen.pl
+++ lab04/lpn/ch5/acclen.pl
@@ -1,14 +1,14 @@
-acclen([],Acc,Length):-
+accLen([],Acc,Length):-
     Length = Acc.
 
-acclen([_|L],OldAcc,Length):-
+accLen([_|L],OldAcc,Length):-
     NewAcc is OldAcc + 1,
-    acclen(L,NewAcc,Length).
+    accLen(L,NewAcc,Length).
 
 len2(List,Length):-
-    acclen(List,0,Length).
+    accLen(List,0,Length).
 
-%% ?-acclen([a,b,c],0,Len).
+%% ?-accLen([a,b,c],0,Len).
 %% Len=3.
 
 %% ?-len2([a,b,c],Len).
